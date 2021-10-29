abc7975016a92e92f92c0b4a943c173160a70aa8
stla <laurent_step@yahoo.fr>
2021-10-29 23:33:22 +0200

    Malfatti circles

--- planegeometry/planegeometry/geometry.py
+++ planegeometry/planegeometry/geometry.py
@@ -1,6 +1,20 @@
-from math import acos, sqrt
+from math import acos, sqrt, tan
 import numpy as np
 from .internal import distance_, vlength_, dot_
+
+
+class Circle:
+    def __init__(self, center, radius):
+        self.center = np.asarray(center, dtype=float)
+        self.radius = radius
+        
+    def __str__(self):
+        return str(self.__dict__)
+
+    def show(self):
+        print("Circle:\n")
+        print(" center: ", tuple(self.center), "\n")
+        print(" radius: ", self.radius, "\n")
 
 
 class Triangle:
@@ -8,6 +22,9 @@ class Triangle:
         self.A = np.asarray(A, dtype=float)
         self.B = np.asarray(B, dtype=float)
         self.C = np.asarray(C, dtype=float)
+        
+    def __str__(self):
+        return str(self.__dict__)
 
     def show(self):
         print("Triangle:\n")
@@ -118,9 +135,72 @@ class Triangle:
         areaABC = sqrt(s*(s-a)*(s-b)*(s-c))
         center = (a*A + b*B + c*C) / p
         radius = areaABC / s
-        return (center, radius)
-
+        return Circle(center, radius)
     
+    def malfatti_circles(self):
+        if self.flatness() == 1:
+            return None
+        A = self.A
+        B = self.B
+        C = self.C
+        a = distance_(B, C)
+        b = distance_(A, C)
+        c = distance_(B, A)
+        p = a + b + c
+        s = p / 2
+        smina = s - a
+        sminb = s - b
+        sminc = s - c
+        areaABC = sqrt(s * smina * sminb * sminc)
+        I = (a*A + b*B + c*C) / p # incenter
+        r = areaABC / s # inradius
+        # radii of Malfatti circles ####
+        IA = vlength_(I - A)
+        IB = vlength_(I - B)
+        IC = vlength_(I - C)
+        halfr = r / 2
+        sminr = s - r
+        r1 = halfr * (sminr - (IB+IC-IA)) / smina
+        r2 = halfr * (sminr - (IC+IA-IB)) / sminb
+        r3 = halfr * (sminr - (IA+IB-IC)) / sminc
+        # centers of Malfatti circles ####
+        d1 = r1 / tan(acos(dot_(C-A, B-A)/b/c)/2)
+        d2 = r2 / tan(acos(dot_(C-B, A-B)/a/c)/2)
+        d3 = r3 / tan(acos(dot_(A-C, B-C)/b/a)/2)
+        w = d1 + d2 + 2*sqrt(r1*r2)
+        u = d2 + d3 + 2*sqrt(r2*r3)
+        v = d3 + d1 + 2*sqrt(r3*r1)
+        d = sqrt((-u+v+w)*(u+v-w)*(u-v+w)*(u+v+w)) / 2
+        x = d/r1 - (v+w)
+        y = u
+        z = u # trilinear coordinates
+        O1 = (u*x*A + v*y*B + w*z*C) / (u*x + v*y + w*z)
+        x = v
+        y = d/r2 - (u+w)
+        z = v # trilinear coordinates
+        O2 = (u*x*A + v*y*B + w*z*C) / (u*x + v*y + w*z)
+        x = w
+        y = w
+        z = d/r3 - (u+v) # trilinear coordinates
+        O3 = (u*x*A + v*y*B + w*z*C) / (u*x + v*y + w*z)
+        circles = {
+          "cA": Circle(center = O1, radius = r1),
+          "cB": Circle(center = O2, radius = r2),
+          "cC": Circle(center = O3, radius = r3)
+        }
+        O2_O3 = O3 - O2
+        O1_O3 = O3 - O1
+        O1_O2 = O2 - O1
+        tangency_points = {
+          "TA": O2 + r2 / vlength_(O2_O3) * O2_O3,
+          "TB": O1 + r1 / vlength_(O1_O3) * O1_O3,
+          "TC": O1 + r1 / vlength_(O1_O2) * O1_O2
+        }
+        return {
+            "circles": circles,
+            "tangency_points": tangency_points
+        }
+
 
 
         
